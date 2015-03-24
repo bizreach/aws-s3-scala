bb7b2e9919760a322825fca7cc272ade5323e181
Tomofumi Tanaka <tomofumi.tanaka@bizreach.co.jp>
2015-03-24 16:11:37 +0900

    Support CredentialsProvider

--- src/main/scala/jp/co/bizreach/s3scala/S3.scala
+++ src/main/scala/jp/co/bizreach/s3scala/S3.scala
@@ -1,9 +1,15 @@
 package jp.co.bizreach.s3scala
 
+import awscala.CredentialsProvider
+
 object S3 {
 
   def apply(accessKeyId: String, secretAccessKey: String)(implicit region: awscala.Region): awscala.s3.S3 = {
     awscala.s3.S3(accessKeyId, secretAccessKey)
+  }
+
+  def apply(credentialsProvider: CredentialsProvider)(implicit region: awscala.Region): awscala.s3.S3 = {
+    awscala.s3.S3(credentialsProvider)
   }
 
   def apply()(implicit region: awscala.Region): awscala.s3.S3 = {
